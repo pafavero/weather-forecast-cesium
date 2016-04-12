17be16616217fa53a0667a6a1ca2270f0dce146c
pafavero <pa.favero@gmail.com>
2016-04-12 23:44:08 +0200

    Add new project

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1>Simulation<br/>wheather forecast app</h1>
+<h2>based on <a href="https://cesiumjs.org/">Cesium Js</a>, a free alternative to Google Earth</h2>
+<p>Since the Google Earth API has been deprecated and not supported after 2016, Cesium can become a good alternatives.</p>
+<p>This prototyp is very simple, but it will be not difficult to implement a more complex application with the use of real data supplied by free/paied weather service.</p>
+<p>For example, <a href="http://openweathermap.org/">OpenWeatherMap</a> provides current weather data (available in JSON, XML) for any location on Earth</p>
+
+<a href="https://cdn.rawgit.com/pafavero/photosphere/master/index.html" target="_blank" >
+    <img title="show preview" src="custom/img/screenshot.JPG" />
+</a>
+<p>
+    <a href="https://cdn.rawgit.com/pafavero/photosphere/master/index.html" target="_blank" >Show preview</a>
+</p>  
---
+++ custom/main.js
@@ -0,0 +1,129 @@
+var Weather = {};
+Weather.main = {
+  symbols: [],
+  count: 0,
+  init: function () {
+    this.viewer = new Cesium.Viewer('cesiumContainer', {
+      baseLayerPicker: false,
+      animation: false,
+      fullscreenButton: false,
+      homeButton: false,
+      infoBox: false,
+      timeline: false,
+      scene3DOnly: true
+    });
+    this.addSymbols();
+    this.planFly(this.count);
+  },
+  fly: function (position, duration, height, fileDownload) {
+    var _this = this;
+    this.viewer.camera.flyTo({
+      duration: duration,
+      destination: Cesium.Cartesian3.fromDegrees(position[0], position[1] - (fileDownload ? 5 : 3), height),
+      orientation: {
+        heading: 0,
+        pitch: -Cesium.Math.PI_OVER_THREE,
+        roll: 0.0
+      },
+      complete: function () {
+        _this.count++;
+        _this.planFly(_this.count);
+        if (fileDownload) {
+          _this.addDataFromFile(fileDownload);
+        }
+      }
+    });
+  },
+  fly2: function (position, duration, height, fileDownload) {
+    var _this = this;
+    this.viewer.camera.flyTo({
+      duration: duration,
+      destination: Cesium.Cartesian3.fromDegrees(position[0], position[1] - 5, height),
+      orientation: {
+        heading: 0,
+        pitch: -0.5,
+        roll: 0.0
+      },
+      complete: function () {
+        _this.count++;
+        _this.planFly(_this.count);
+        if (fileDownload) {
+          _this.addDataFromFile(fileDownload);
+        }
+      }
+    });
+  },
+  planFly: function (count) {
+    var _this = this;
+    if (count === 0) {
+      this.fly([12, 39], 6, 1000000, 'dataI');
+    } else if (count === 1) {
+      setTimeout(function () {
+        _this.fly([9.3, 43.5], 4, 1000000, null);
+      }, 1000);
+    } else if (count === 2) {
+      this.fly([2, 44], 4, 1000000, 'dataFR');
+    } else if (count === 3) {
+      setTimeout(function () {
+        _this.fly([2, 46], 2, 1000000, null);
+      }, 1000);
+    } else if (count === 4) {
+      this.fly2([-3, 52], 4, 300000, 'dataUK');
+    }
+  },
+  addLabel: function (val, position, sym) {
+    if (sym) {
+      this.viewer.entities.add({
+        position: Cesium.Cartesian3.fromDegrees(position[0], position[1]),
+        billboard: this.symbols[sym]
+      });
+    } else {
+      this.viewer.entities.add({
+        position: Cesium.Cartesian3.fromDegrees(position[0], position[1]),
+        billboard: {
+          image: 'custom/img/icon-temperature.png',
+          scale: .5,
+          verticalOrigin: Cesium.VerticalOrigin.BOTTOM
+        },
+        label: {
+          text: val + '\u00b0',
+          horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
+          verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+          pixelOffset: new Cesium.Cartesian2(0.0, -30.0),
+          eyeOffset: new Cesium.Cartesian3(0, 0, -100)
+        }
+      });
+    }
+  },
+  addSymbols: function () {
+    this.symbols['Clouds'] = new Cesium.BillboardGraphics();
+    this.symbols['Clouds'].image = new Cesium.ConstantProperty('custom/img/cloud.png');
+    this.symbols['Clouds'].scale = 0.8;
+
+    this.symbols['Clear'] = new Cesium.BillboardGraphics();
+    this.symbols['Clear'].image = new Cesium.ConstantProperty('custom/img/sun.png');
+    this.symbols['Clear'].scale = 0.8;
+
+    this.symbols['Mist'] = new Cesium.BillboardGraphics();
+    this.symbols['Mist'].image = new Cesium.ConstantProperty('custom/img/cloud.png');
+    this.symbols['Mist'].scale = 0.8;
+
+    this.symbols['Rain'] = new Cesium.BillboardGraphics();
+    this.symbols['Rain'].image = new Cesium.ConstantProperty('custom/img/rain.png');
+    this.symbols['Rain'].scale = 0.8;
+  },
+  addDataFromFile: function (name) {
+    var _this = this;
+    $.ajax({
+      dataType: "json",
+      url: 'custom/data/' + name + '.json',
+      success: function (data) {
+        $.each(data.list, function (index, value) {
+          _this.addLabel(value.main.temp.toFixed(1), [value.coord.lon, value.coord.lat],
+              value.isShowSym ? value.weather[0].main : null);
+        });
+      }
+    });
+  }
+};
+
